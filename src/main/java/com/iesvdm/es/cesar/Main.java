4b8a7603726bc8549631a6ef647eaaab145d86e9
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2024-10-15 22:34:08 +0200

    Package Cesar

---
+++ src/main/java/com/iesvdm/es/cesar/Main.java
@@ -0,0 +1,29 @@
+package com.iesvdm.es.cesar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Supplier<String> test = () -> " Hola Mundo";
+        System.out.println(test.get());
+
+        Supplier<Integer> number = () -> (int) (Math.random() * 10);
+        System.out.println(number.get());
+
+
+        ArrayList<String> listas = new ArrayList<>();
+        List<String> names2 = new ArrayList<>(Arrays.asList("C", "D", "A", "B", "F", "G", "H", "E", "I", "J"));
+
+        names2.stream()
+                .sorted()
+                .map(String::toLowerCase)
+                .forEach(listas::add);
+        System.out.println(listas);
+
+
+    }
+}
