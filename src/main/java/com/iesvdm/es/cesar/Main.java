f58076b792758beace8a8116adb21ba56e97c225
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2024-10-19 19:33:08 +0200

    ordenar por longitud

--- src/main/java/com/iesvdm/es/cesar/Main.java
+++ src/main/java/com/iesvdm/es/cesar/Main.java
@@ -8,22 +8,21 @@ import java.util.function.Supplier;
 public class Main {
     public static void main(String[] args) {
 
-        Supplier<String> test = () -> " Hola Mundo";
-        System.out.println(test.get());
-
-        Supplier<Integer> number = () -> (int) (Math.random() * 10);
-        System.out.println(number.get());
-
-
-        ArrayList<String> listas = new ArrayList<>();
-        List<String> names2 = new ArrayList<>(Arrays.asList("C", "D", "A", "B", "F", "G", "H", "E", "I", "J"));
-
-        names2.stream()
-                .sorted()
-                .map(String::toLowerCase)
-                .forEach(listas::add);
-        System.out.println(listas);
-
+//        Supplier<String> test = () -> " Hola Mundo";
+//        System.out.println(test.get());
+//
+//        Supplier<Integer> number = () -> (int) (Math.random() * 10);
+//        System.out.println(number.get());
+//
+//
+//        ArrayList<String> listas = new ArrayList<>();
+//        List<String> names2 = new ArrayList<>(Arrays.asList("C", "D", "A", "B", "F", "G", "H", "E", "I", "J"));
+//
+//        names2.stream()
+//                .sorted()
+//                .map(String::toLowerCase)
+//                .forEach(listas::add);
+//        System.out.println(listas);
 
     }
 }
---
+++ src/main/java/com/iesvdm/es/cesar/ejercicios/OrdenaLista.java
@@ -0,0 +1,62 @@
+package com.iesvdm.es.cesar.ejercicios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OrdenaLista {
+    public static void main(String[] args) {
+
+
+        /**
+         * Dada una lista de números enteros filtra por numeros pares y
+         * ordenarla de menor a mayor. Y en otro caso, de mayor a menor.
+         * Utilizar streams.
+         */
+
+        // De menor a mayor
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> numerosPares =
+                numeros.stream()
+                        .filter(n -> n % 2 == 0)
+                        .sorted()
+                        .toList();
+        System.out.println(numerosPares);
+
+        // De mayor a menor
+        List<Integer> numeros2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> numeroPares2 =
+                numeros2.stream()
+                        .filter(n -> n % 2 == 0)
+                        .sorted(Comparator.reverseOrder())
+                        .toList();
+        System.out.println(numeroPares2);
+
+
+        /**
+         * Transformar y agrupar una lista de palabras
+         * Dada una lista de palabras,
+         * convierte cada palabra a mayúsculas y agrúpalas por su longitud.
+         * */
+
+        List<String> palabras = Arrays.asList("stream", "java", "lambda", "function", "code");
+        Map<Integer, List<String>> agrupadas = palabras.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.groupingBy(String::length));
+        System.out.println(agrupadas);
+
+
+        /**
+         * Procesar una lista de objetos:
+         * Dada una lista de objetos Persona con atributos nombre y edad,
+         * filtra las personas mayores de 18 años,
+         * ordena por edad y convierte la lista resultante en un mapa
+         * donde la clave sea el nombre y el valor sea la edad.
+         * */
+
+    }
+}
